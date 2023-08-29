4393b81750b9b47d4f4381c444d6b092d7dc6280
Vaibhav <vaibhavsatish9@gmail.com>
2023-08-29 23:44:53 +0530

    logging file added

---
+++ main.py
@@ -0,0 +1,12 @@
+#from mlProject import logger
+#or
+from src.mlProject import logger
+
+logger.info("Welcome to our custom logging")
+
+
+#It will give you logging info in your terminal
+#Timestamp with date. Also will give you the module name and the message
+
+#This will also create a folder 'logs' inside which it has created a running_logs.log, where you will be able to see all your log history
+#This is good to catch errors when you run into them.
